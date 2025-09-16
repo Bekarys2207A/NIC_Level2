379135d32c3ef9a77acfe1c1fbf960f28242d987
Bekarys2207A <bekarysabdumutalip@gmail.com>
2025-09-16 21:26:37 +0500

    Initial commit

---
+++ tasks/views.py
@@ -0,0 +1,12 @@
+from rest_framework import viewsets, filters
+from .models import Task
+from .serializers import TaskSerializer
+
+# Create your views here.
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['status'] 
+    
